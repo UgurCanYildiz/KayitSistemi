6d8214155d2a4b0aa44c5b707d7d62565d635501
Uğur Can <101288809+UgurCanYildiz@users.noreply.github.com>
2022-03-31 20:53:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,39 @@
+class Musteri():
 
+    def __init__(self, musteriAdi, musteriCinsiyet, musteriTc, musteriDogumYili, musteriAdres):
+        self.musteriAdi = musteriAdi
+        self.musteriCinsiyet = musteriCinsiyet
+        self.musteriTc = musteriTc
+        self.musteriDogumYili = musteriDogumYili
+        self.musteriAdres = musteriAdres
 
+    def BilgileriYazdir(self):
+        print(" İsminiz : ", self.musteriAdi, "\n",
+              "Cinsiyetiniz : ", self.musteriCinsiyet, "\n",
+              "Tc No : ", self.musteriTc, "\n",
+              "Doğum Yılınız : ", self.musteriDogumYili, "\n",
+              "Adresiniz : ", self.musteriAdres, "\n"
+              )
+        print("\n")
 
+    def BilgileriKontrolEt(self):
+
+        while True:
+            cevap = int(input("Doğru İse = 1 Yanlış İse = 2 Basınız : "))
+            if (cevap == 1):
+                print('Bilgileriniz Tanımlanmıştır ...')
+                break
+            elif (cevap == 2):
+                print('Tekrar Deneyiniz ! ')
+                self.musteriAdi = input('İsim Ve Soyisminiz  : ')
+                self.musteriCinsiyet = input('Cinsiyetiniz (Erkek = E , Kadın = K):  ')
+                self.musteriTc = input("Tc'niz :  ")
+                self.musteriDogumYili = input("Doğum Yılınız  : ")
+                self.musteriAdres = input("Adresiniz : ")
+                print("Doğru İse = 'E' Yanlış İse = 'Y' Basınız : ")
+                Musteri.BilgileriYazdir(self)
+            else:
+                print("Yanlış operatör")
 
 
 MusteriAdi = input('İsim Ve Soyisminiz  : ')
@@ -9,32 +42,8 @@ MusteriTc = input("Tc'niz :  ")
 MusteriDogumYili = input("Doğum Yılınız  : ")
 MusteriAdresi = input("Adresiniz : ")
 
+musteri1 = Musteri(MusteriAdi, MusteriCinsiyet, MusteriTc, MusteriDogumYili, MusteriAdresi)
 
-def BilgileriYazdir(MusteriAdi , MusteriCinsiyet , MusteriTc , MusteriDogumYili , MusteriAdresi):
-      print(" İsminiz : ", MusteriAdi, "\n",
-            "Cinsiyetiniz : ", MusteriCinsiyet, "\n",
-            "Tc No : ", MusteriTc, "\n",
-            "Doğum Yılınız : ", MusteriDogumYili, "\n",
-            "Adresiniz : ", MusteriAdresi, "\n"
-            )
-      print("\n")
-def BilgileriKontrolEt() :
-      print("Doğru İse = 'E' Yanlış İse = 'Y' Basınız : ")
-      cevap = str(input())
-      while 1 :
-            if cevap == 'E' or 'e':
-                  print('Bilgileriniz Tanımlanmıştır . ')
-                  break
-            elif cevap == 'Y' or 'y':
-                  print('Tekrar Deneyiniz ! ')
-                  MusteriAdi = input('İsim Ve Soyisminiz  : ')
-                  MusteriCinsiyet = input('Cinsiyetiniz (Erkek = E , Kadın = K):  ')
-                  MusteriTc = input("Tc'niz :  ")
-                  MusteriDogumYili = input("Doğum Yılınız  : ")
-                  MusteriAdresi = input("Adresiniz : ")
-                  print("Doğru İse = 'E' Yanlış İse = 'Y' Basınız : ")
-                  cevap = str(input())
+musteri1.BilgileriYazdir()
 
-BilgileriYazdir(MusteriAdi , MusteriCinsiyet , MusteriTc , MusteriDogumYili , MusteriAdresi )
-BilgileriKontrolEt()
-print("Tekrar Bekleriz ...")
+musteri1.BilgileriKontrolEt()
